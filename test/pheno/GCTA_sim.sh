556ce36491cecad17b77eefc9e5d3b2901d92e8e
Geyu Zhou <eldronzhou@gmail.com>
2021-05-30 16:41:50 -0400

    Change column name of test input

--- test/pheno/GCTA_sim.sh
+++ test/pheno/GCTA_sim.sh
@@ -7,7 +7,7 @@ gcta64 --bfile ../genotype/eur_chr22 --simu-qt --simu-causal-loci ../effect_size
 plink2 --bfile ../genotype/eur_chr22 --pheno ./sim_1.phen --glm a0-ref cols=chrom,pos,alt,ref,a1freq,orbeta,se,tz,p,nobs --out ../summary_stat/sim_1
 
 # format conversion
-awk '{print $3,$6,$4,$9,$12}' ../summary_stat/sim_1.PHENO1.glm.linear > ../summary_stat/sim_1.txt
+awk '{BEGIN{print "SNP" "A1" "A2" "BETA" "P"} NR>1 {print $3,$6,$4,$9,$12}}' ../summary_stat/sim_1.PHENO1.glm.linear > ../summary_stat/sim_1.txt
 
 # clean-up
 rm -f ../summary_stat/sim_1.PHENO1.glm.linear ../summary_stat/*.log ../summary_stat/*.id 
